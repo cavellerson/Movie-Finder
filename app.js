e1761b064b09cbeb5b40eeaea2896806df17aafd
Cavell Wong <cavellerson@Cavells-MacBook-Air.local>
2020-12-08 11:28:27 -0500

    Done with Project For now...

--- app.js
+++ app.js
@@ -9,8 +9,7 @@
     $('.misc-info').empty();
     $('.movie-plot').empty();
     $('#add').remove();
-    $('input[type="text"]').val('');
-    $('.main-container').css('background-color', 'rgba(0, 0, 0, 0.5)');
+
 
 
 
@@ -18,7 +17,8 @@
         url:'https://www.omdbapi.com/?apikey=53aa2cd6&t=' + userInput
     }).then(
         (data)=>{
-
+            $('input[type="text"]').val('');
+            $('.main-container').css('background-color', 'rgba(0, 0, 0, 0.5)');
             if (data.Response === "False" || data.Error === "Movie not found!") {
               alert('Movie not found!');
               $("#userInput").val('');
@@ -70,9 +70,6 @@
       }
       $('.playlist-images').children().eq(currentImgIndex).css('display', 'block');
 
-
-      // console.log(`currentImgIndex: ${currentImgIndex}\n numOfImages: ${numOfImages}`);
-      // console.log('this is previous button, current index is at: ' + currentImgIndex);
     })
 
     $('#next-button').on('click', () => {
@@ -82,8 +79,6 @@
         currentImgIndex = 0;
       }
       $('.playlist-images').children().eq(currentImgIndex).css('display', 'block');
-      // console.log(`currentImgIndex: ${currentImgIndex}\n numOfImages: ${numOfImages}`);
-      // console.log("this is next button, current index is at: " + currentImgIndex);
     })
 
     $('#clear-button').on('click', () => {
@@ -97,112 +92,19 @@
 
 
 
+        //////////////////////////
+        //key press for enter ///
+        ////////////////////////
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//////////////////////////
-// search bar w/ enter //
-////////////////////////
-
-// how to make the search bar function via enter key and pressing search?
-///how to get search bar to work once we hit enter?
-// used the key press function but the function doesn't work with ajax...
-
-    // $("#userInput").keypress(function (e) {
-    //   if (e.keyCode === 13) {
-    //   alert('Enter key pressed!');
-    //   const userInput = $('input[type="text"]').val();
-    //   event.preventDefault();
-    //   $.ajax({
-    //       url:'https://www.omdbapi.com/?apikey=53aa2cd6&t=' + userInput
-    //   }).then(
-    //       (data)=>{
-    //         console.log(data.Title);
-    //           if (data.Response === "False" || data.Error === "Movie not found!") {
-    //             alert('invalid movie');
-    //             $("#userInput").val('');
-    //           }
-    //           else {
-    //             console.log(data);
-    //             const $img = $('<img>').attr('src', data.Poster).appendTo('.movie-img');
-    //
-    //             const $title = $('<h1>').text(`${data.Title} (${data.Year})`).appendTo(".movie-title");
-    //             const $rated = $('<h2>').text(`Rated: ${data.Rated}`).appendTo(".misc-info");
-    //             const $runtime = $('<h2>').text(`${data.Runtime}`).appendTo(".misc-info");
-    //             const $genre = $('<h2>').text(`${data.Genre}`).appendTo(".misc-info");
-    //             const $metaScore = $('<h2>').text(`Meta Score: ${data.Metascore}`).appendTo(".misc-info");
-    //             const $moviePlot = $('<p>').text(data.Plot).appendTo(".movie-info");
-    //             $("#userInput").val('');
-    //             $('#add').on('click', () => {
-    //             $('.movie-playlist>h1').css("display", "block");
-    //             $img.appendTo(".movie-playlist");
-    //             $('.movie-title').empty();
-    //             $('.movie-info').empty();
-    //             $('#add').remove();
-    //           }
-    //
-    //
-    //           // const $movieRelease =
-    //
-    //       },
-    //       ()=>{
-    //           console.log("couldn't access the API");
-    //       }
-    //   );
-    //   }
-    // });
-
-
-
-    //////////////////////////
-    //key press for enter ///
-    ////////////////////////
-
-
-    // $('#userInput').keypress(
-    //   function(event){
-    //   let keycode = (event.keyCode ? event.keyCode : event.which);
-    //
-    //   if(keycode === 13){
-    //     alert('You pressed a "enter" key in textbox');
-    //
-    //
-    //   }
-    //
-    // });
+        // $('#userInput').keypress(
+        //   function(event){
+        //   let keycode = (event.keyCode ? event.keyCode : event.which);
+        //
+        //   if(keycode === 13){
+        //     alert('You pressed a "enter" key in textbox');
+        //
+        //
+        //   }
+        //
+        // });
